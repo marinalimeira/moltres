4837d1dee75506ff7bee04784a19ec5457039d98
Marina Limeira <marinaflessa@gmail.com>
2015-09-13 22:22:57 -0300

    Add http return

--- lib/moltres.rb
+++ lib/moltres.rb
@@ -1,5 +1,9 @@
 require "moltres/version"
 
 module Moltres
-  # Your code goes here...
+  class Application
+    def call(env)
+      [ 200, { 'Content-Type' => 'text/html'}, ['Hello from Ruby on Moltres!']]
+    end
+  end
 end

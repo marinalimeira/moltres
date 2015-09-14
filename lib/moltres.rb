b28dedc3de642e314e487e8fd0ca6336158aebd3
Marina Limeira <marinaflessa@gmail.com>
2015-09-13 23:03:33 -0300

    Add debug file on Application

--- lib/moltres.rb
+++ lib/moltres.rb
@@ -3,6 +3,7 @@ require "moltres/version"
 module Moltres
   class Application
     def call(env)
+      `echo debug > debug.txt`;
       [ 200, { 'Content-Type' => 'text/html'}, ['Hello from Ruby on Moltres!']]
     end
   end
